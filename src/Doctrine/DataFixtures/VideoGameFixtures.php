b4a10b5ef53face5972099dbb6ae590e23796140
Christophe BEAUQUEL <c.beauquel@neobook.fr>
2025-02-26 14:38:59 +0100

    Installation du projet

--- src/Doctrine/DataFixtures/VideoGameFixtures.php
+++ src/Doctrine/DataFixtures/VideoGameFixtures.php
@@ -2,17 +2,19 @@
 
 namespace App\Doctrine\DataFixtures;
 
-use App\Model\Entity\User;
-use App\Model\Entity\VideoGame;
-use App\Rating\CalculateAverageRating;
-use App\Rating\CountRatingsPerValue;
+use Faker\Generator;
 use DateTimeImmutable;
+use App\Model\Entity\Tag;
+use App\Model\Entity\User;
+use App\Model\Entity\Review;
+use App\Model\Entity\VideoGame;
+use function array_fill_callback;
+use App\Rating\CountRatingsPerValue;
+use App\Rating\CalculateAverageRating;
+
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Persistence\ObjectManager;
-use Faker\Generator;
-
-use function array_fill_callback;
 
 final class VideoGameFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -23,27 +25,68 @@ final class VideoGameFixtures extends Fixture implements DependentFixtureInterfa
     ) {
     }
 
+    public const KEYWORDS = ['multijoueur', 'solo', 'en ligne', 'hors ligne', 'coopératif', 'compétitif'];
+
     public function load(ObjectManager $manager): void
     {
         $users = $manager->getRepository(User::class)->findAll();
 
-        $videoGames = array_fill_callback(0, 50, fn (int $index): VideoGame => (new VideoGame)
-            ->setTitle(sprintf('Jeu vidéo %d', $index))
+        $tags = [];
+
+        // Création des tags
+        foreach (self::KEYWORDS as $keyword) {
+            $tag = (new Tag())->setName($keyword);
+            $manager->persist($tag);
+            $this->addReference('tag_' . $keyword, $tag);
+            $tags[] = $tag;
+        }
+
+        $videoGames = [];
+        // $videoGames = array_fill_callback(0, 50, fn (int $index): VideoGame => (new VideoGame)
+        for($i = 0; $i < 50; $i++){
+        $videoGame = (new VideoGame())
+            ->setTitle(sprintf('Jeu vidéo %d', $i))
             ->setDescription($this->faker->paragraphs(10, true))
             ->setReleaseDate(new DateTimeImmutable())
             ->setTest($this->faker->paragraphs(6, true))
-            ->setRating(($index % 5) + 1)
-            ->setImageName(sprintf('video_game_%d.png', $index))
-            ->setImageSize(2_098_872)
-        );
+            ->setRating(($i % 5) + 1)
+            ->setImageName(sprintf('video_game_%d.png', $i))
+            ->setImageSize(2_098_872);
+
+            $randomTags = $this->faker->randomElements($tags, rand(1, 3));
+                // Ajout aléatoire de tags
+                foreach ($randomTags as $tag) {
+                    $videoGame->addTag($tag);
+                }
+
+            $this->calculateAverageRating->calculateAverage($videoGame);
+            $this->countRatingsPerValue->countRatingsPerValue($videoGame);
+
+            $manager->persist($videoGame);
+
+            $videoGames[] = $videoGame;
+        };
+
 
         // TODO : Ajouter les tags aux vidéos
 
-        array_walk($videoGames, [$manager, 'persist']);
+        // array_walk($videoGames, [$manager, 'persist']);
 
         $manager->flush();
 
         // TODO : Ajouter des reviews aux vidéos
+        for ($i = 0; $i < 75; $i++){
+            $review = (new Review())
+                ->setUser($this->faker->randomElement($users))
+                ->setVideoGame($this->faker->randomElement($videoGames))
+                ->setRating($this->faker->numberBetween(1, 5))
+                ->setComment($this->faker->paragraphs(1, true));
+            $manager->persist($review);
+
+        }
+
+        $manager->flush();
+
 
     }
 
---
+++ tests/Unit/CalculateAverageRatingTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Model\Entity\VideoGame;
+use PHPUnit\Framework\TestCase;
+
+class CalculateAverageRatingTest extends TestCase
+{
+    public function testAverageRatingCalcul(): void
+    {
+        
+        $this->assertTrue(true);
+    }
+}
